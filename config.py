f6a44c2baa5d86a1c54c8ac7832b0f8ee18f8cc1
gnudelman <gnudelman@sdd.com.ar>
2024-10-25 01:01:00 -0300

    sss

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ class Config:
             cls._instance = super(Config, cls).__new__(cls)
             # Initialize configuration values here
             cls._instance.default_path = ".\\data"
-            cls._instance.default_audio_ext = ".mp4"  #  ".m4a"
+            cls._instance.default_audio_ext = ".mp4"  #  ".m4a" 
             cls._instance.default_txt_ext = ".txt"
             cls._instance.api_key = "your-api-key..."
-            cls._instance.openAi_key =  "nada1111222"
+            cls._instance.openAi_key =  "nada1111222ggg"
 
         return cls._instance
 
